505ebee012acd79651bd72dae31de1a12886b304
kolaster123 <carbajalruben1563@gmail.com>
2024-09-13 10:56:46 -0700

    template complete

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// Import the style
+import '/style.css';
+
+// Button functionality for social links
+document.getElementById('linkedinBtn').addEventListener('click', () => {
+  window.open('https://www.linkedin.com/in/ruben-carbajal-585705227/', '_blank');
+});
+
+document.getElementById('githubBtn').addEventListener('click', () => {
+  window.open('https://github.com/kolaster123', '_blank');
+});
